f33d0593e4ed2be7b45b721978f3b329911b8f1c
Samuel <torressam333@gmail.com>
2022-07-16 11:05:03 -0700

    Configure gulp to compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
-function defaultTask(callBack) {
-  callBack();
-}
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-exports.default = defaultTask;
+// Compile sass to css and output in a css file
+const compileStyles = () => {
+  return src('styles.scss').pipe(sass()).pipe(dest('css'));
+};
+
+// Re-compile when styles change
+const watchSass = () => {
+  // File to watch
+  watch(['styles.scss'], compileStyles);
+};
+
+exports.default = series(compileStyles, watchSass);
