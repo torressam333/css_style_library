a2034ad2b0fd1d83d9dc788479a68fc26173a82e
Samuel <torressam333@gmail.com>
2022-07-19 16:10:05 -0700

    Break out variables into separate file

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const sass = require('gulp-sass')(require('sass'));
 // Compile sass to css and output in a css DIR in the root dir
 const compileStyles = () => {
   // https://nodejs.org/api/stream.html#stream_readable_pipe_destination_options
-  return src('styles.scss').pipe(sass()).pipe(dest('css'));
+  return src('./resources/sass/styles.scss').pipe(sass()).pipe(dest('css'));
 };
 
 // Re-compile when styles change
 const watchSass = () => {
   // File to watch with delay to ensure performant
-  watch(['styles.scss'], compileStyles);
+  watch(['./resources/sass/styles.scss'], compileStyles);
 };
 
 exports.default = series(compileStyles, watchSass);
