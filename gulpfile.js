106318018cf0a3cb035a500e57513eb0e5578881
Samuel <torressam333@gmail.com>
2022-07-18 18:38:36 -0700

    Minor tweaks to gulp config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-// Compile sass to css and output in a css file
+// Compile sass to css and output in a css DIR in the root dir
 const compileStyles = () => {
+  // https://nodejs.org/api/stream.html#stream_readable_pipe_destination_options
   return src('styles.scss').pipe(sass()).pipe(dest('css'));
 };
 
 // Re-compile when styles change
 const watchSass = () => {
-  // File to watch
-  watch(['styles.scss'], compileStyles);
+  // File to watch with delay to ensure performant updates
+  watch(['styles.scss'], { delay: 200 }, compileStyles);
 };
 
 exports.default = series(compileStyles, watchSass);
