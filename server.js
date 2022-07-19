0c0da19b82e9e8707fea332f09e34a7ac590a47d
Samuel <torressam333@gmail.com>
2022-07-19 16:35:30 -0700

    Make node server DRY-er

--- server.js
+++ server.js
@@ -1,37 +1,25 @@
 let http = require('http');
 let fs = require('fs');
 
+const determineContentType = (contentType, resp, filePath) => {
+  fs.readFile(filePath, null, (error, payload, filePath) => {
+    resp.setHeader('Content-Type', contentType);
+
+    if (error) {
+      resp.writeHead(404);
+      resp.write('Oooohh noooo, where the heck did I put that file');
+    }
+
+    resp.end(payload, 'utf-8');
+  });
+};
+
 const server = (request, response) => {
-  let filePath = `.${request.url}`;
   //this is either '/' or '/css/styles.css'...will need to update for images later;
-  console.log(request.url);
-
   if (request.url === '/' || request.url === '/index.html') {
-    filePath = './index.html';
-
-    fs.readFile(filePath, null, (error, payload) => {
-      response.setHeader('Content-Type', 'text/html');
-
-      if (error) {
-        response.writeHead(404);
-        respone.write('Oooohh noooo, where the heck did I put that file');
-      }
-
-      response.end(payload, 'utf-8');
-    });
+    determineContentType('text/html', response, './index.html');
   } else if (request.url === '/css/styles.css') {
-    filePath = './css/styles.css';
-
-    fs.readFile(filePath, null, (error, payload) => {
-      response.setHeader('Content-Type', 'text/css');
-
-      if (error) {
-        response.writeHead(404);
-        respone.write('Oooohh noooo, where the heck did I put that file');
-      }
-
-      response.end(payload, 'utf-8');
-    });
+    determineContentType('text/css', response, '/css/styles.css');
   }
 };
 
